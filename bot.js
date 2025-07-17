70daccb400ce93b8490287882f4af52d267ab75d
mhmdmhdy10A <mohamedgaming2024t@gmail.com>
2025-07-17 10:43:30 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,8 @@ const mineflayer = require('mineflayer');
 
 function createBot() {
   const bot = mineflayer.createBot({
-    host: process.env.MC_HOST || 'jasemn.aternos.me',
-    port: parseInt(process.env.MC_PORT) || 26405,
+    host: process.env.MC_HOST || 'emerald.magmanode.com',
+    port: parseInt(process.env.MC_PORT) || 33815,
     username: process.env.MC_USER || 'AFK_Bot_24h'
   });
 
